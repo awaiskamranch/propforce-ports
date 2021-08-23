fb0145e206da4922f563e25053c06b18f4c9d39e
Awais Kamran <awaiskamran@ITs-MacBook-Pro.local>
2021-08-23 10:03:46 +0500

    propforce-ports-node: feat(Application Structure) package lock file updated.

--- startup/routes.js
+++ startup/routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
+var cors = require("cors");
 const port = require("../routes/Port");
 const user = require("../routes/User");
 
 module.exports = function (app) {
   app.use(express.json());
+  app.use(cors());
   app.use("/api/user", user);
   app.use("/api/port", port);
 };
