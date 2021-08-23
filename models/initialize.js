fc108063635a23653955a0c28400c04dd2523c0d
Awais Kamran <awaiskamran@ITs-MacBook-Pro.local>
2021-08-23 11:15:51 +0500

    propforce-ports-node: feat(Application Structure) initialize.js updated.

--- models/initialize.js
+++ models/initialize.js
@@ -1,5 +1,5 @@
-const { generateUserModel } = require("./models/user/user.schema");
-const { generatePortModel } = require("./models/port/port.schema");
+const { generateUserModel } = require("./user/user.schema");
+const { generatePortModel } = require("./port/port.schema");
 
 function generateModels() {
   const modelTobeGenerated = [generateUserModel(), generatePortModel()];
